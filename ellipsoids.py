6c0adfb01fbd8547cb2dc9ad0d70bc7bb38b679a
ced4rtree <usernamesarenoteasy@proton.me>
2025-07-02 16:44:38 -0600

    everything is supposed to work. it doesnt tho

--- ellipsoids.py
+++ ellipsoids.py
@@ -1,29 +1,37 @@
 #!/usr/bin/env python
+
+# compilation command on NixOS:
+# bash -c 'conda-shell -c "conda activate flowermd && python ellipsoids.py"'
+
 from flowermd.base import Pack, Lattice, Simulation
 from flowermd.library import EllipsoidForcefield, EllipsoidChain, PPS, OPLS_AA_PPS
 from flowermd.utils import get_target_box_number_density, get_target_box_mass_density
-from flowermd.utils.constraints import create_rigid_ellipsoid_chain
+from time import sleep
+from typing import Tuple, List
+from util import *
 import argparse
 import datetime
 import gsd
 import gsd.hoomd
 import hoomd
-import matplotlib.pyplot as plt
 import math
+import matplotlib.pyplot as plt
 import numpy as np
 import os
+import sys
 import unyt as u
 import warnings
-from util import *
 
 warnings.filterwarnings('ignore')
 
-R_CUT = 3.0
+RESOLUTION = 100
+PROGRESS_BAR_WIDTH = 20
+
+R_CUT = 10.0
 EPSILON = 1.0
 
-MAX_DIST = R_CUT*1.1
+MAX_DIST = 5.0
 MIN_DIST = 0.1
-RESOLUTION = int(1e2)
 TIME_STRING = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 OUTPUT_DIR = 'output/' + TIME_STRING + '/'
 
@@ -33,75 +41,205 @@ except OSError as error:
     print('failed to create directory ' + OUTPUT_DIR)
     exit(1)
 
-potential = np.array([])
-radius = np.array([])
+# ellipsoids oriented like ||
+parallel_potential = np.array([])
+parallel_radius = np.array([])
 
-for i in range(RESOLUTION+1): # +1 for inclusive range
-    gsd_file_name = OUTPUT_DIR + f'trajectory-{i}.gsd'
-    log_file_name = OUTPUT_DIR + f'log-{i}.txt'
+# ellipsoids oriented like <><>
+parallel_long_potential = np.array([])
+parallel_long_radius = np.array([])
 
-    ellipsoid = EllipsoidChain(num_mols=2, lpar=1.0, bead_mass=1.0, lengths=1)
-    system = Pack(density=0.00001*u.Unit("nm**-3"), molecules=ellipsoid)
+# ellipsoids oriented like |<>
+perpendicular_potential = np.array([])
+perpendicular_radius = np.array([])
 
-    ellipsoid_dist = ((MAX_DIST-MIN_DIST)/(RESOLUTION))*i+MIN_DIST
+class Direction(Enum):
+    X = 0
+    Y = 1
+    Z = 2
 
-    ellipsoid_to_origin(system.system.children[0].children[0])
-    ellipsoid_to_origin(system.system.children[1].children[0])
-    translate_ellipsoid_by(system.system.children[1].children[0], [0.0, ellipsoid_dist, 0.0])
-    system.gmso_system = system._convert_to_gmso()
+def simulate(
+        direction: Direction,
+        name: str,
+        rotation: np.ndarray = None,
+        orientations: np.ndarray = None,
+) -> Tuple[List[float], List[float]]:
+    '''Do many simulations of two ellipsoids, where the first is located at (0,
+        0, 0) and parallel to the x-axis, and the other is placed at RESOLUTION
+        points between MIN_DIST and MAX_DIST away from the first along the
+        specified direction, and rotated around the x, y, and z axis relative to
+        its center.
 
-    ff = EllipsoidForcefield(
-        epsilon=EPSILON,
-        lpar=1.0,
-        lperp=0.5,
-        r_cut=R_CUT,
-        bond_r0=0.1,
-        bond_k=100
+    Parameters
+    ----------
+    rotation: np.ndarray, An array of rotation values in radians, formatted like
+    [x, y, z]
+
+    direction: Direction, The direction for the simulation to sample potentials
+    in
+    name: str, The name that will be used for identifying logs
+
+    Return Value
+    ------------
+    Returns an array of input radii and corresponding array of output potential
+    values
+
+    '''
+
+    potential = [None] * (RESOLUTION)
+    radius = [None] * (RESOLUTION)
+
+    for i in range(RESOLUTION):
+        gsd_file_name = OUTPUT_DIR + f'{name}-{i}.gsd'
+        log_file_name = OUTPUT_DIR + f'{name}-{i}.txt'
+        
+        ellipsoid = EllipsoidChain(num_mols=2, lpar=1.0, bead_mass=1.0, lengths=1)
+        system = Pack(density=0.1*u.Unit("nm**-3"), molecules=ellipsoid)
+
+        # scaled to be MIN_DIST and MAX_DIST inclusive while using only
+        # RESOLUTION many steps
+        max_dist_scaled = MAX_DIST
+        dist_step = ((MAX_DIST-MIN_DIST)/RESOLUTION)
+        max_dist_scaled += dist_step
+        ellipsoid_dist = ((max_dist_scaled-MIN_DIST)/(RESOLUTION))*i+MIN_DIST
+        
+        ellipsoid_to_origin(system.system.children[0].children[0])
+        ellipsoid_to_origin(system.system.children[1].children[0])
+        translation = [0.0, 0.0, 0.0]
+        translation[direction.value] = ellipsoid_dist
+        translate_ellipsoid_by(system.system.children[1].children[0], translation)
+        if rotation is not None:
+            rotate_ellipsoid_by(system.system.children[1].children[0], rotation)
+        system.gmso_system = system._convert_to_gmso()
+        system._hoomd_snapshot = system._create_hoomd_snapshot()
+
+        print(f"System positions before: {system.system.children[1].children[0].children}\n")
+        
+        ff = EllipsoidForcefield(
+            epsilon=EPSILON,
+            lpar=1.0,
+            lperp=0.5,
+            r_cut=R_CUT,
+            bond_r0=0.1,
+            bond_k=100
+        )
+        rigid_frame, rigid_constraint = create_rigid_ellipsoid_chain(
+            system.hoomd_snapshot,
+            orientations=orientations
+        )
+
+        print(f"Rigid frame positions after: {rigid_frame.particles.position}\n")
+        
+        ellipsoid_sim = Simulation(
+            initial_state=rigid_frame,
+            forcefield=ff.hoomd_forces,
+            constraint=rigid_constraint,
+            gsd_write_freq=1,
+            gsd_file_name=gsd_file_name,
+            log_write_freq=1,
+            log_file_name=log_file_name,
+            dt=0.001
+        )
+        
+        ellipsoid_sim.run_NVT(n_steps=0, kT=1.0, tau_kt=1.0, thermalize_particles=False)
+        
+        ellipsoid_sim.flush_writers()
+        
+        ellipsoid_gsd(
+            gsd_file=gsd_file_name,
+            new_file=gsd_file_name.replace('.gsd', '-ovito.gsd'),
+            ellipsoid_types='R',
+            lpar=1.0,
+            lperp=0.5,
+        )
+        
+        log = np.genfromtxt(log_file_name, names=True)
+        potential_entry = log["mdcomputeThermodynamicQuantitiespotential_energy"]
+
+        radius[i] = ellipsoid_dist
+        potential[i] = potential_entry
+        
+        # print progress bar
+        # https://stackoverflow.com/questions/3002085/how-to-print-out-status-bar-and-percentage
+        print(f"potential_entry: {potential_entry}\nradius_entry: {ellipsoid_dist}\nlog file: {log_file_name}")
+        sys.stdout.write('\r')
+        sys.stdout.write(f"[%-{PROGRESS_BAR_WIDTH}s] %d%%\n\n" % ('='*int(i/(RESOLUTION/PROGRESS_BAR_WIDTH)), 100/RESOLUTION*i))
+        sys.stdout.flush()
+
+    return radius, potential
+
+if __name__ == '__main__':
+    # ||
+    parallel = simulate(
+        Direction.Y,
+        "parallel",
     )
-    rigid_frame, rigid_constraint = create_rigid_ellipsoid_chain(
-        system.hoomd_snapshot
+    parallel_radius = np.append(parallel_radius, parallel[0])
+    parallel_potential = np.append(parallel_potential, parallel[1])
+    
+    # <><>
+    parallel_long = simulate(
+        Direction.X,
+        "parallel_long",
     )
-
-    ellipsoid_sim = Simulation(
-        initial_state=rigid_frame,
-        forcefield=ff.hoomd_forces,
-        constraint=rigid_constraint,
-        gsd_write_freq=1,
-        gsd_file_name=gsd_file_name,
-        log_write_freq=1,
-        log_file_name=log_file_name,
-        dt=0.001
+    parallel_long_radius = np.append(parallel_long_radius, parallel_long[0])
+    parallel_long_potential = np.append(parallel_long_potential, parallel_long[1])
+    
+    # |<>
+    perpendicular = simulate(
+        Direction.Y,
+        "perpendicular",
+        rotation = np.array([0, 0, np.pi/2]),
+        orientations = np.array([[1, 0, 0, 0], rotate_quaternion([1, 0, 0, 0], np.pi/2, 'z')]),
     )
+    perpendicular_radius = np.append(perpendicular_radius, perpendicular[0])
+    perpendicular_potential = np.append(perpendicular_potential, perpendicular[1])
 
-    ellipsoid_sim.run_NVT(n_steps=0, kT=1.0, tau_kt=1.0, thermalize_particles=False)
+    print(f"parallel radius && parallel_long radius: {parallel_radius == parallel_long_radius}")
+    print(f"perpendicular radius && parallel_long radius: {perpendicular_radius == parallel_long_radius}")
+    print(f"perpendicular radius && parallel radius: {perpendicular_radius == parallel_radius}")
+    print(f"parallel potential && parallel_long potential: {parallel_potential == parallel_long_potential}")
+    print(f"perpendicular potential && parallel_long potential: {perpendicular_potential == parallel_long_potential}")
+    print(f"perpendicular potential && parallel potential: {perpendicular_potential == parallel_potential}")
+    
+    plt.plot(parallel_radius, parallel_potential/2, label='Parallel (||)')
+    plt.plot(parallel_long_radius, parallel_long_potential/2, label='Parallel Long (<><>)')
+    plt.plot(perpendicular_radius, perpendicular_potential/2, label='Perpendicular (|<>)')
+    plt.title('Potential Energy Per Particle vs. Radius')
+    plt.xlabel('Radius')
+    plt.ylabel('Potential Energy Per Particle')
+    plt.legend()
+    plt.savefig(f'{OUTPUT_DIR}potential-energy-per.png')
+    plt.close()
+    
+    plt.plot(parallel_radius, parallel_potential, label='Parallel (||)')
+    plt.plot(parallel_long_radius, parallel_long_potential, label='Parallel Long (<><>)')
+    plt.plot(perpendicular_radius, perpendicular_potential, label='Perpendicular (|<>)')
+    plt.title('Potential Energy vs. Radius')
+    plt.xlabel('Radius')
+    plt.ylabel('Potential Energy')
+    plt.legend()
+    plt.savefig(f'{OUTPUT_DIR}potential-energy.png')
+    plt.close()
+    
+    plt.plot(parallel_radius, parallel_potential, label='Parallel (||)')
+    plt.plot(parallel_long_radius, parallel_long_potential, label='Parallel Long (<><>)')
+    plt.plot(perpendicular_radius, perpendicular_potential, label='Perpendicular (|<>)')
+    plt.title('Potential Energy vs. Radius')
+    plt.xlabel('Radius')
+    plt.ylabel('Potential Energy')
+    plt.ylim(-EPSILON-1, 2)
+    plt.legend()
+    plt.savefig(f'{OUTPUT_DIR}potential-energy-limited.png')
+    plt.close()
 
-    ellipsoid_sim.flush_writers()
-
-    ellipsoid_gsd(
-        gsd_file=gsd_file_name,
-        new_file=gsd_file_name.replace('.gsd', '-ovito.gsd'),
-        ellipsoid_types='R',
-        lpar=1.0,
-        lperp=0.5,
-    )
-
-    log = np.genfromtxt(log_file_name, names=True)
-    potential_entry = log["mdcomputeThermodynamicQuantitiespotential_energy"]
-    potential = np.append(potential, potential_entry)
-    radius = np.append(radius, ellipsoid_dist)
-    print(f"potential_entry: {potential_entry}, radius_entry: {ellipsoid_dist}")
-
-plt.plot(radius, potential/2)
-plt.title('Potential Energy Per Particle vs. Radius')
-plt.xlabel('Radius')
-plt.ylabel('Potential Energy Per Particle')
-plt.savefig(f'{OUTPUT_DIR}potential-energy.png')
-plt.close()
-
-plt.plot(radius, potential/2)
-plt.title('Potential Energy Per Particle vs. Radius')
-plt.xlabel('Radius')
-plt.ylabel('Potential Energy Per Particle')
-plt.ylim(-EPSILON-1, 2)
-plt.savefig(f'{OUTPUT_DIR}potential-energy-limited.png')
-plt.close()
+    plt.plot(parallel_radius, parallel_potential/2, label='Parallel (||)')
+    plt.plot(parallel_long_radius, parallel_long_potential/2, label='Parallel Long (<><>)')
+    plt.plot(perpendicular_radius, perpendicular_potential/2, label='Perpendicular (|<>)')
+    plt.title('Potential Energy Per Particle vs. Radius')
+    plt.xlabel('Radius')
+    plt.ylabel('Potential Energy Per Particle')
+    plt.ylim(-EPSILON-1, 2)
+    plt.legend()
+    plt.savefig(f'{OUTPUT_DIR}potential-energy-limited-per.png')
+    plt.close()
--- util.py
+++ util.py
@@ -1,5 +1,23 @@
-import numpy as np
+from enum import Enum
+from flowermd.base import Pack, Lattice, Simulation
+from flowermd.library import EllipsoidForcefield, EllipsoidChain, PPS, OPLS_AA_PPS
+from flowermd.utils import get_target_box_number_density, get_target_box_mass_density
+from flowermd.utils.constraints import create_rigid_ellipsoid_chain
+from time import sleep
+from util import *
+import argparse
+import datetime
+import gsd
 import gsd.hoomd
+import hoomd
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import rowan
+import sys
+import unyt as u
+import warnings
 
 def translate_ellipsoid_by(ellipsoid, translation):
     '''
@@ -64,16 +82,16 @@ def ellipsoid_to_origin(ellipsoid):
     ellipsoid_to_origin(system.system.children[0].children[0])
     '''
     center = ellipsoid.children[0].pos
+    bond = ellipsoid.children[1].pos
     head = ellipsoid.children[2].pos
-    bond = ellipsoid.children[3].pos
 
     lpar = np.linalg.norm(head - center)
     bond_from_center = np.linalg.norm(bond - center)
 
     ellipsoid.children[0].pos = np.array([0.0, 0.0, 0.0])
-    ellipsoid.children[1].pos = np.array([-lpar, 0.0, 0.0])
-    ellipsoid.children[2].pos = np.array([lpar, 0.0, 0.0])
-    ellipsoid.children[3].pos = np.array([bond_from_center, 0.0, 0.0])
+    ellipsoid.children[1].pos = np.array([bond_from_center, 0.0, 0.0])
+    ellipsoid.children[2].pos = np.array([-lpar, 0.0, 0.0])
+    ellipsoid.children[3].pos = np.array([lpar, 0.0, 0.0])
 
 def ellipsoid_gsd(gsd_file, new_file, ellipsoid_types, lpar, lperp):
     """Add needed information to GSD file to visualize ellipsoids.
@@ -110,8 +128,151 @@ def ellipsoid_gsd(gsd_file, new_file, ellipsoid_types, lpar, lperp):
                         }
                     else:
                         shapes_dict = {"type": "Sphere", "diameter": 0.001}
-                        shape_dicts_list.append(shapes_dict)
-                        snap.particles.type_shapes = shape_dicts_list
-                        snap.validate()
-                        new_t.append(snap)
+                    shape_dicts_list.append(shapes_dict)
+                snap.particles.type_shapes = shape_dicts_list
+                snap.validate()
+                new_t.append(snap)
 
+def create_rigid_ellipsoid_chain(snapshot, orientations = None):
+    """Create rigid bodies from a snapshot.
+
+    This is designed to be used with flowerMD's built in library
+    for simulating ellipsoidal chains.
+    As a result, this will not work for setting up rigid bodies
+    for other kinds of systems.
+
+    See `flowermd.library.polymer.EllipsoidChain` and
+    `flowermd.library.forcefields.EllipsoidForcefield`.
+
+    Parameters
+    ----------
+    snapshot : gsd.hoomd.Snapshot; required
+        The snapshot of the system.
+        Pass in `flowermd.base.System.hoomd_snapshot()`.
+
+    Returns
+    -------
+    rigid_frame : gsd.hoomd.Frame
+        The snapshot of the rigid bodies.
+    rigid_constrain : hoomd.md.constrain.Rigid
+        The rigid body constrain object.
+
+    """
+    bead_len = 4  # Number of particles belonging to 1 rigid body
+    typeids = snapshot.particles.typeid.reshape(-1, bead_len)
+    matches = np.where((typeids == typeids))
+    rigid_const_idx = (matches[0] * bead_len + matches[1]).reshape(-1, bead_len)
+    n_rigid = rigid_const_idx.shape[0]  # number of ellipsoid monomers
+
+    rigid_masses = []
+    rigid_pos = []
+    rigid_moi = []
+    # Find the mass, position and MOI for reach rigid center
+    for idx in rigid_const_idx:
+        mass = np.sum(np.array(snapshot.particles.mass)[idx])
+        pos = snapshot.particles.position[idx][0]
+        rigid_masses.append(mass)
+        rigid_pos.append(pos)
+        rigid_moi.append([0, 2, 2])
+
+    rigid_frame = gsd.hoomd.Frame()
+    rigid_frame.particles.types = ["R"] + snapshot.particles.types
+    rigid_frame.particles.N = n_rigid + snapshot.particles.N
+    rigid_frame.particles.typeid = np.concatenate(
+        (([0] * n_rigid), snapshot.particles.typeid + 1)
+    )
+    rigid_frame.particles.mass = np.concatenate(
+        (rigid_masses, snapshot.particles.mass)
+    )
+    rigid_frame.particles.position = np.concatenate(
+        (rigid_pos, snapshot.particles.position)
+    )
+    rigid_frame.particles.moment_inertia = np.concatenate(
+        (rigid_moi, np.zeros((snapshot.particles.N, 3)))
+    )
+    
+    if orientations is not None:
+        # initialize orientation to correct dimensions
+        rigid_frame.particles.orientation = [orientations[0]]
+
+        # rigid body orientations
+        if len(orientations) > 1:
+            for orientation in orientations[1:]:
+                rigid_frame.particles.orientation = np.vstack((rigid_frame.particles.orientation, orientation))
+
+        # individual particle orientations
+        for orientation in orientations:
+            for i in range(4): # one orientation per particle that matches rigid body orientation
+                rigid_frame.particles.orientation = np.vstack((rigid_frame.particles.orientation, orientation))
+
+        print(f"ORIENTATIONS: {rigid_frame.particles.orientation}")
+    else:
+        rigid_frame.particles.orientation = [[1, 0, 0, 0]] * (
+            n_rigid + snapshot.particles.N
+        )
+        
+    rigid_frame.particles.body = np.concatenate(
+        (
+            np.arange(n_rigid),
+            np.arange(n_rigid).repeat(rigid_const_idx.shape[1]),
+        )
+    )
+    rigid_frame.configuration.box = snapshot.configuration.box
+
+    # set up bonds
+    if snapshot.bonds.N > 0:
+        rigid_frame.bonds.N = snapshot.bonds.N
+        rigid_frame.bonds.types = snapshot.bonds.types
+        rigid_frame.bonds.typeid = snapshot.bonds.typeid
+        rigid_frame.bonds.group = [
+            list(np.add(g, n_rigid)) for g in snapshot.bonds.group
+        ]
+    # set up angles
+    if snapshot.angles.N > 0:
+        rigid_frame.angles.N = snapshot.angles.N
+        rigid_frame.angles.types = snapshot.angles.types
+        rigid_frame.angles.typeid = snapshot.angles.typeid
+        rigid_frame.angles.group = [
+            list(np.add(g, n_rigid)) for g in snapshot.angles.group
+        ]
+
+    # find local coordinates of the particles in the first rigid body
+    # only need to find the local coordinates for the first rigid body
+    local_coords = (
+        snapshot.particles.position[rigid_const_idx[0]] - rigid_pos[0]
+    )
+
+    rigid_constrain = hoomd.md.constrain.Rigid()
+    rigid_constrain.body["R"] = {
+        "constituent_types": ["X", "A", "T", "T"],
+        "positions": local_coords,
+        "orientations": [[1, 0, 0, 0]] * len(local_coords),
+    }
+    return rigid_frame, rigid_constrain
+
+# Credit to marjanalbooyeh for writing this
+# https://github.com/cmelab/ellipsoid_orientations/blob/main/src/quaternions.py
+def rotate_quaternion(q, theta, axis):
+    cos_theta_2 = np.cos(theta / 2)
+    sin_theta_2 = np.sin(theta / 2)
+
+    if axis == 'x':
+        r = [cos_theta_2, sin_theta_2, 0, 0]
+    elif axis == 'y':
+        r = [cos_theta_2, 0, sin_theta_2, 0]
+    elif axis == 'z':
+        r = [cos_theta_2, 0, 0, sin_theta_2]
+    else:
+        raise ValueError("Axis must be 'x', 'y', or 'z'")
+
+    q0, q1, q2, q3 = q
+    r0, r1, r2, r3 = r
+
+    q_prime = [
+        r0 * q0 - r1 * q1 - r2 * q2 - r3 * q3,
+        r0 * q1 + r1 * q0 + r2 * q3 - r3 * q2,
+        r0 * q2 - r1 * q3 + r2 * q0 + r3 * q1,
+        r0 * q3 + r1 * q2 - r2 * q1 + r3 * q0
+    ]
+
+    return rowan.normalize(q_prime)                
