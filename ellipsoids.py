80ffb854dcb7eae69838282a005857b2ac1e3108
ced4rtree <usernamesarenoteasy@proton.me>
2025-07-07 15:08:11 -0600

    chat... hoomd ain't working

--- ellipsoids.py
+++ ellipsoids.py
@@ -16,6 +16,7 @@ import gsd.hoomd
 import hoomd
 import math
 import matplotlib.pyplot as plt
+import mbuild as mb
 import numpy as np
 import os
 import sys
@@ -33,7 +34,7 @@ LPAR=1.0
 LPERP=0.5
 
 MAX_DIST = 5.0
-MIN_DIST = 0.5
+MIN_DIST = 0.1
 TIME_STRING = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 OUTPUT_DIR = 'output/' + TIME_STRING + '/'
 
@@ -63,8 +64,7 @@ class Direction(Enum):
 def simulate(
         direction: Direction,
         name: str,
-        rotation: np.ndarray = None,
-        orientations: np.ndarray = None,
+        orientation: np.ndarray = None,
 ) -> Tuple[List[float], List[float]]:
     '''Do many simulations of two ellipsoids, where the first is located at (0,
         0, 0) and parallel to the x-axis, and the other is placed at RESOLUTION
@@ -74,12 +74,13 @@ def simulate(
 
     Parameters
     ----------
-    rotation: np.ndarray, An array of rotation values in radians, formatted like
-    [x, y, z]
-
     direction: Direction, The direction for the simulation to sample potentials
     in
+
     name: str, The name that will be used for identifying logs
+
+    orientation: np.ndarray[float, float, float, float], Quaternion representing
+    the orientation of the second ellipsoid
 
     Return Value
     ------------
@@ -95,86 +96,89 @@ def simulate(
         gsd_file_name = OUTPUT_DIR + f'{name}-{i}.gsd'
         log_file_name = OUTPUT_DIR + f'{name}-{i}.txt'
         
-        ellipsoid = EllipsoidChain(num_mols=2, lpar=LPAR, bead_mass=1.0, lengths=1)
-        system = Pack(density=0.1*u.Unit("nm**-3"), molecules=ellipsoid)
-
         # scaled to be MIN_DIST and MAX_DIST inclusive while using only
         # RESOLUTION many steps
         max_dist_scaled = MAX_DIST
         dist_step = ((MAX_DIST-MIN_DIST)/RESOLUTION)
         max_dist_scaled += dist_step
         ellipsoid_dist = ((max_dist_scaled-MIN_DIST)/(RESOLUTION))*i+MIN_DIST
-        
-        ellipsoid_to_origin(system.system.children[0].children[0])
-        ellipsoid_to_origin(system.system.children[1].children[0])
+
+        # create system and molecules
         translation = [0.0, 0.0, 0.0]
         translation[direction.value] = ellipsoid_dist
-        translate_ellipsoid_by(system.system.children[1].children[0], translation)
-        if rotation is not None:
-            rotate_ellipsoid_by(system.system.children[1].children[0], rotation)
-        system.gmso_system = system._convert_to_gmso()
-        system._hoomd_snapshot = system._create_hoomd_snapshot()
+        box = mb.Compound()
+        ellipsoid_one = mb.Compound(name="A", pos=(0, 0, 0), mass=1.0)
+        ellipsoid_two = mb.Compound(name="A", pos=translation, mass=1.0)
+        # if rotation is not None:
+        #     ellipsoid_two.rotate(theta=rotation, around=ellipsoid_two.center)
+        
+        # dummy molecules to expand the box
+        box.add(ellipsoid_one)
+        box.add(ellipsoid_two)
+        snapshot = mb.conversion.to_hoomdsnapshot(box)
+        snapshot.configuration.box = [100, 100, 100, 0, 0, 0]
+        
+        # add Gay-Berne forcefield to the integrator
+        nlist = hoomd.md.nlist.Cell(buffer=2.0, exclusions=["body"])
+        gb = hoomd.md.pair.aniso.GayBerne(nlist=nlist, default_r_cut=R_CUT)
+        gb.params[("A", "A")] = dict(
+            epsilon=1.0, lperp=0.5, lpar=1.0
+        )
+        
+        integrator = hoomd.md.Integrator(dt=0.001)
+        integrator.forces.append(gb)
+        
+        # add NVT method to integrator
+        nvt = hoomd.md.methods.ConstantVolume(
+            filter=hoomd.filter.All(), thermostat=hoomd.md.methods.thermostats.Bussi(kT=1.5)
+        )
+        integrator.methods.append(nvt)
+        
+        simulation = hoomd.Simulation(device=hoomd.device.CPU(), seed=1)
+        simulation.create_state_from_snapshot(snapshot)
+        simulation.operations.integrator = integrator
+        
+        # compute thermodynamic properties
+        thermodynamic_properties = hoomd.md.compute.ThermodynamicQuantities(
+            filter=hoomd.filter.All()
+        )
+        simulation.operations.computes.append(thermodynamic_properties)
 
-        print(f"System positions before: {system.system.children[1].children[0].children}\n")
-        
-        ff = EllipsoidForcefield(
-            epsilon=EPSILON,
-            lpar=LPAR,
-            lperp=LPERP,
-            r_cut=R_CUT,
-            bond_r0=0.1,
-            bond_k=100
-        )
-        rigid_frame, rigid_constraint = create_rigid_ellipsoid_chain(
-            system.hoomd_snapshot,
-            orientations=orientations
-        )
+        with simulation._state.cpu_local_snapshot as data:
+            if orientation is not None:
+                data.particles.orientation[1] = orientation
 
-        print(f"Rigid frame positions after: {rigid_frame.particles.position}\n")
-        
-        ellipsoid_sim = Simulation(
-            initial_state=rigid_frame,
-            forcefield=ff.hoomd_forces,
-            constraint=rigid_constraint,
-            gsd_write_freq=1,
-            gsd_file_name=gsd_file_name,
-            log_write_freq=1,
-            log_file_name=log_file_name,
-            dt=0.001
-        )
-        
-        ellipsoid_sim.run_NVT(n_steps=1, kT=1.0, tau_kt=1.0, thermalize_particles=False)
-        
-        ellipsoid_sim.flush_writers()
-        
+        simulation.run(0)
+
+        # flush GSD writer
+        if os.path.exists(gsd_file_name):
+            os.remove(gsd_file_name)
+        hoomd.write.GSD.write(state=simulation.state, filename=gsd_file_name, mode="xb")
         ellipsoid_gsd(
             gsd_file=gsd_file_name,
             new_file=gsd_file_name.replace('.gsd', '-ovito.gsd'),
-            ellipsoid_types='R',
+            ellipsoid_types='A',
             lpar=LPAR,
             lperp=LPERP,
         )
-        
-        log = np.genfromtxt(log_file_name, names=True)
-        potential_entry = log["mdcomputeThermodynamicQuantitiespotential_energy"]
 
-        radius[i] = ellipsoid_dist
-        potential[i] = potential_entry
-        
         # print progress bar
         # https://stackoverflow.com/questions/3002085/how-to-print-out-status-bar-and-percentage
-        print(f"potential_entry: {potential_entry}\nradius_entry: {ellipsoid_dist}\nlog file: {log_file_name}")
+        print(f"potential_entry: {thermodynamic_properties.potential_energy}\nradius_entry: {ellipsoid_dist}\nlog file: {log_file_name}")
         sys.stdout.write('\r')
         sys.stdout.write(f"[%-{PROGRESS_BAR_WIDTH}s] %d%%\n\n" % ('='*int(i/(RESOLUTION/PROGRESS_BAR_WIDTH)), 100/RESOLUTION*i))
         sys.stdout.flush()
 
-    return radius, potential[0], potential[1]
+        radius[i] = ellipsoid_dist
+        potential[i] = thermodynamic_properties.potential_energy
+
+    return radius, potential
 
 if __name__ == '__main__':
     # ||
     parallel = simulate(
         Direction.Y,
-        "parallel_0",
+        "parallel",
     )
     parallel_radius = np.append(parallel_radius, parallel[0])
     parallel_potential = np.append(parallel_potential, parallel[1])
@@ -182,7 +186,7 @@ if __name__ == '__main__':
     # <><>
     parallel_long = simulate(
         Direction.X,
-        "parallel_long_0",
+        "parallel_long",
     )
     parallel_long_radius = np.append(parallel_long_radius, parallel_long[0])
     parallel_long_potential = np.append(parallel_long_potential, parallel_long[1])
@@ -190,37 +194,11 @@ if __name__ == '__main__':
     # |<>
     perpendicular = simulate(
         Direction.Y,
-        "perpendicular_0",
-        rotation = np.array([0, 0, np.pi/2]),
-        orientations = np.array([[1, 0, 0, 0], rotate_quaternion([1, 0, 0, 0], np.pi/2, 'z')]),
+        "perpendicular",
+        orientation = rotate_quaternion([1, 0, 0, 0], np.pi/2, 'z'),
     )
     perpendicular_radius = np.append(perpendicular_radius, perpendicular[0])
     perpendicular_potential = np.append(perpendicular_potential, perpendicular[1])
-
-    parallel = simulate(
-        Direction.Y,
-        "parallel_1",
-    )
-    parallel_radius = np.append(parallel_radius, parallel[0])
-    parallel_potential = np.append(parallel_potential, parallel[2])
-    
-    # <><>
-    parallel_long = simulate(
-        Direction.X,
-        "parallel_long_1",
-    )
-    parallel_long_radius = np.append(parallel_long_radius, parallel_long[0])
-    parallel_long_potential = np.append(parallel_long_potential, parallel_long[2])
-    
-    # |<>
-    perpendicular = simulate(
-        Direction.Y,
-        "perpendicular_1",
-        rotation = np.array([0, 0, np.pi/2]),
-        orientations = np.array([[1, 0, 0, 0], rotate_quaternion([1, 0, 0, 0], np.pi/2, 'z')]),
-    )
-    perpendicular_radius = np.append(perpendicular_radius, perpendicular[0])
-    perpendicular_potential = np.append(perpendicular_potential, perpendicular[2])
 
     print(f"parallel radius && parallel_long radius: {parallel_radius == parallel_long_radius}")
     print(f"perpendicular radius && parallel_long radius: {perpendicular_radius == parallel_long_radius}")
